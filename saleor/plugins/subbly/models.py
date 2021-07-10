743f8be01606c497e4e8cc45ccf8272bd7b05f8c
Gerard Arguelles <gerard.arguelles@gmail.com>
2021-07-11 04:43:03 +0800

    [feature] generate invite code

--- saleor/plugins/subbly/models.py
+++ saleor/plugins/subbly/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -7,6 +9,7 @@ class SubblySubscription(models.Model):
     email = models.CharField(max_length=255, default="")
     subscription_id = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True, null=True)
+    invite_code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     modified = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
