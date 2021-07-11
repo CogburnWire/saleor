3383f6c6c603278a9ac67fe6293a09d7db5659fd
Gerard Arguelles <gerard.arguelles@gmail.com>
2021-07-11 18:37:55 +0800

    [chore] subbly subscription admin

--- saleor/plugins/subbly/admin.py
+++ saleor/plugins/subbly/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SubblySubscription
+
+
+@admin.register(SubblySubscription)
+class SubblySubscriptionAdmin(admin.ModelAdmin):
+    fields = ("id", "email", "subscription_id")
